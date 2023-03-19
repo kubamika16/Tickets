16462deb6b4927e2bd59cba44f03e5c95c4f658a
kubamika16 <kuba.mika16@gmail.com>
2023-03-19 16:29:48 +0000

    commit

--- aws.js
+++ aws.js
@@ -1,10 +1,10 @@
 // Required modules
-const AWS = require('aws-sdk')
-const fs = require('fs')
-const path = require('path')
-require('dotenv').config()
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
-const ticketBucket = 'concert-data-bucket-2023'
+const ticketBucket = "concert-data-bucket-2023";
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -16,89 +16,89 @@ const ticketBucket = 'concert-data-bucket-2023'
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  region: 'eu-west-2',
-})
+  region: "eu-west-2",
+});
 AWS.config.update({
-  region: 'eu-west-2',
-  endpoint: 'https://dynamodb.eu-west-2.amazonaws.com',
+  region: "eu-west-2",
+  endpoint: "https://dynamodb.eu-west-2.amazonaws.com",
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
-})
+});
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // DynamoDB
-const docClient = new AWS.DynamoDB.DocumentClient()
-const table = 'tickets'
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = "tickets";
 
 // Function to fetch all items from the DynamoDB table
 const getAllItems = async () => {
   const params = {
     TableName: table,
-  }
+  };
 
   try {
-    const data = await docClient.scan(params).promise()
+    const data = await docClient.scan(params).promise();
     // console.log('Retrieved items:', JSON.stringify(data.Items, null, 2))
-    return data.Items
+    return data.Items;
   } catch (err) {
     console.error(
-      'Unable to retrieve items. Error JSON:',
-      JSON.stringify(err, null, 2),
-    )
-    return null
+      "Unable to retrieve items. Error JSON:",
+      JSON.stringify(err, null, 2)
+    );
+    return null;
   }
-}
+};
 
 // Example usage:
 // getAllItems()
 
 // Function to fetch all keys from the DynamoDB table
 const getAllKeys = async () => {
-  const items = await getAllItems()
+  const items = await getAllItems();
   if (items !== null) {
-    const keys = items.map((item) => item.name)
+    const keys = items.map((item) => item.name);
     // console.log('Retrieved keys:', JSON.stringify(keys, null, 2))
-    return keys
+    return keys;
   } else {
-    console.error('Unable to retrieve keys.')
-    return null
+    console.error("Unable to retrieve keys.");
+    return null;
   }
-}
+};
 
 // Example usage:
 // getAllKeys()
 
 // Function that uploads an item to the DynamoDB table
 const uploadItem = async (folder, file) => {
-  const item = await processFile(folder, file)
+  const item = await processFile(folder, file);
   // console.log('Processed file: ', item)
 
   const params = {
     TableName: table,
     Item: item,
-  }
+  };
 
   try {
-    await docClient.put(params).promise()
-    console.log(`Added item with ID: ${item.name}`)
+    await docClient.put(params).promise();
+    console.log(`Added item with ID: ${item.name}`);
   } catch (err) {
     console.error(
-      'Unable to add item. Error JSON:',
-      JSON.stringify(err, null, 2),
-    )
+      "Unable to add item. Error JSON:",
+      JSON.stringify(err, null, 2)
+    );
   }
-}
+};
 
 // Function to process a single JSON file
 const processFile = async (folder, file) => {
-  const filePath = path.join(folder, file)
-  const fileContent = await fs.promises.readFile(filePath)
-  const item = JSON.parse(fileContent)
-  return item
-}
+  const filePath = path.join(folder, file);
+  const fileContent = await fs.promises.readFile(filePath);
+  const item = JSON.parse(fileContent);
+  return item;
+};
 
 // Function to delete an item by its 'name' attribute
 const deleteItem = async (itemName) => {
@@ -107,61 +107,61 @@ const deleteItem = async (itemName) => {
     Key: {
       name: itemName,
     },
-  }
+  };
 
   try {
-    await docClient.delete(params).promise()
-    console.log(`Deleted item with name: ${itemName}`)
+    await docClient.delete(params).promise();
+    console.log(`Deleted item with name: ${itemName}`);
   } catch (error) {
     console.error(
-      'Unable to delete item. Error JSON:',
-      JSON.stringify(error, null, 2),
-    )
+      "Unable to delete item. Error JSON:",
+      JSON.stringify(error, null, 2)
+    );
   }
-}
+};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Main function to read the folder, filter JSON files and process each one
 const dynamoDBFunction = async (folder) => {
   // File that is not in folder, but still in DynamoDB and needs to be deleted
-  let deleteFile
+  let deleteFile;
   ////////////////////////////////////////////////////////////////////////////////
   // FOLDER
   // Reads the contents of the folder
-  const concertNames = []
+  const concertNames = [];
 
-  const filesInFolder = await fs.promises.readdir(folder)
+  const filesInFolder = await fs.promises.readdir(folder);
   // Filter onlu JSON files
   const jsonFiles = filesInFolder.filter(
-    (file) => path.extname(file) === '.json',
-  )
+    (file) => path.extname(file) === ".json"
+  );
   for (const file of jsonFiles) {
-    const processedFile = await processFile(folder, file)
-    concertNames.push(processedFile.name)
+    const processedFile = await processFile(folder, file);
+    concertNames.push(processedFile.name);
   }
   // console.log('Name: ', concertNames)
 
   ////////////////////////////////////////////////////////////////////////////////
   // "TICKETS" TABLE
-  const dynamoDBKeys = await getAllKeys()
+  const dynamoDBKeys = await getAllKeys();
 
   // DELETE CERTAIN FILES FROM DYNAMO DB
   for (const name of dynamoDBKeys) {
     // Sprawdź 2 tablice
     // Jeśli 'concertNames', czyli tablica z nazwami z folderu nie zawiera nazwy która znajduje się w 'getAllKeys' (tablica z nazwami z Dynamo)
     if (!concertNames.includes(name)) {
-      console.log(`concertNames (items in folder) don't include ${name}`)
+      console.log(`concertNames (items in folder) don't include ${name}`);
 
       // // Usunięcie Rekordu z DynamoDB
-      await deleteItem(name)
+      await deleteItem(name);
     }
   }
 
   // UPLOAD JSON FILES TO DYNAMO DB
   for (const file of jsonFiles) {
-    await uploadItem(folder, file)
+    await uploadItem(folder, file);
   }
-}
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -170,16 +170,16 @@ const dynamoDBFunction = async (folder) => {
 // S3
 const uploadFilesToS3 = async (folder) => {
   // Read contents of the folder
-  const filesInFolder = await fs.promises.readdir(folder)
+  const filesInFolder = await fs.promises.readdir(folder);
   // console.log(filesInFolder)
 
   // List the contents of the S3 bucket
   const objectsInBucket = await s3
     .listObjectsV2({ Bucket: ticketBucket })
-    .promise()
+    .promise();
 
   // Extract the filenames from the S3 objects
-  const filesInBucket = objectsInBucket.Contents.map((object) => object.Key)
+  const filesInBucket = objectsInBucket.Contents.map((object) => object.Key);
 
   // Now, 3 things to do in S3 Bucket: Update existing files, add new files added in the folder, delete files that don't exist in the folder
 
@@ -187,49 +187,49 @@ const uploadFilesToS3 = async (folder) => {
   await Promise.all(
     filesInFolder.map(async (file) => {
       // Jeśli S3 Bucket nie zawiera pliku który jest w folderze
-      if (path.extname(file) === '.json' && !filesInBucket.includes(file)) {
+      if (path.extname(file) === ".json" && !filesInBucket.includes(file)) {
         // Przeczytanie pliku
-        const fileContent = await fs.promises.readFile(`${folder}/${file}`)
+        const fileContent = await fs.promises.readFile(`${folder}/${file}`);
 
         const params = {
-          Bucket: 'concert-data-bucket-2023',
+          Bucket: "concert-data-bucket-2023",
           Key: file,
           Body: fileContent,
-        }
-        await s3.upload(params).promise()
+        };
+        await s3.upload(params).promise();
       }
-    }),
-  )
+    })
+  );
 
   // Delete any files in the bucket that are not in the folder
   await Promise.all(
     objectsInBucket.Contents.filter(
-      (object) => !filesInFolder.includes(object.Key),
+      (object) => !filesInFolder.includes(object.Key)
     ).map(async (object) => {
       const params = {
-        Bucket: 'concert-data-bucket-2023',
+        Bucket: "concert-data-bucket-2023",
         Key: object.Key,
-      }
-      await s3.deleteObject(params).promise()
-    }),
-  )
+      };
+      await s3.deleteObject(params).promise();
+    })
+  );
 
   // Update the contents of any files in the bucket that are also in the folder
   await Promise.all(
     filesInFolder.map(async (file) => {
-      if (path.extname(file) === '.json' && filesInBucket.includes(file)) {
-        const fileContent = await fs.promises.readFile(`${folder}/${file}`)
+      if (path.extname(file) === ".json" && filesInBucket.includes(file)) {
+        const fileContent = await fs.promises.readFile(`${folder}/${file}`);
         const params = {
-          Bucket: 'concert-data-bucket-2023',
+          Bucket: "concert-data-bucket-2023",
           Key: file,
           Body: fileContent,
-        }
-        await s3.upload(params).promise()
+        };
+        await s3.upload(params).promise();
       }
-    }),
-  )
+    })
+  );
 
-  console.log('FILES SUCCESSFULLY UPLOADED, UPDATED, DELETED')
-}
+  console.log("FILES SUCCESSFULLY UPLOADED, UPDATED, DELETED");
+};
 
-module.exports = { uploadFilesToS3, dynamoDBFunction }
+module.exports = { uploadFilesToS3, dynamoDBFunction };
--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,54 +1,39 @@
-// Import the required AWS clients and packages
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
-const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
+const AWS = require("aws-sdk");
+// Region set as in a Lambda Function
+AWS.config.update({
+  region: "eu-west-2",
+});
+// DybamoDB client
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+// My table
+const dynamoDBTableName = "product-inventory";
+// Health path variable
+const healthPath = "/health";
+// All items path
+const itemsPath = "/items";
+// Single item path
+const itemPath = "/item";
 
-// Initialize the DynamoDB client
-const dynamoDBClient = new DynamoDBClient({ region: 'eu-west-2' })
+// Lambda will start executing code because of exports.handler
+// Event param contains info about upcoming event (f.e API request data) depending on a trigger
+exports.handler = async function (event) {
+  console.log("Request event:", event);
 
-// Create an instance of the DynamoDB Document Client to work with DynamoDB tables
-const documentClient = DynamoDBDocument.from(dynamoDBClient)
+  let response;
 
-// This is the main handler function that AWS Lambda will execute
-exports.handler = async (event) => {
-  // Log the received event for debugging purposes
-  console.log('Received event:', JSON.stringify(event, null, 2))
-
-  // Extract the operation and payload from the received event
-  const operation = event.operation
-  const payload = event.payload
-
-  // Define a variable to hold the response object
-  let response
-
-  // Use a switch statement to handle different operations
-  switch (operation) {
-    case 'read':
-      // Call the readItem function with the payload
-      response = await readItem(payload)
-      break
-    default:
-      // If the operation is not recognized, return an error message
-      response = { error: 'Invalid operation' }
+  switch (true) {
+    case event.httpMethod === "GET" && event.path === healthPath:
+      response = buildResponse(200);
+      break;
   }
+};
 
-  // Return the response object
-  return response
-}
-
-// Function to read an item from the DynamoDB table
-async function readItem(payload) {
-  // Define the parameters for the documentClient.get operation
-  const params = {
-    TableName: payload.TableName,
-    Key: payload.Key,
-  }
-
-  // Call the documentClient.get operation and return the result
-  try {
-    const data = await documentClient.get(params)
-    return data.Item
-  } catch (error) {
-    console.error('Error reading item:', error)
-    return { error: 'Error reading item' }
-  }
-}
+const buildResponse = function (statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+};
