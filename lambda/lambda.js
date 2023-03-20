5fda282b69fbd20f791243f0e2bda21cce6a1785
kubamika16 <kuba.mika16@gmail.com>
2023-03-20 15:33:22 +0000

    commit

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,45 +1,47 @@
-const AWS = require('aws-sdk')
+// scanDynamoRecords ?
+
+const AWS = require("aws-sdk");
 // Region set as in a Lambda Function
 AWS.config.update({
-  region: 'eu-west-2',
-})
+  region: "eu-west-2",
+});
 // DybamoDB client
-const dynamodb = new AWS.DynamoDB.DocumentClient()
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 // My table
-const dynamoDBTableName = 'tickets'
+const dynamoDBTableName = "tickets";
 // Health path variable
-const healthPath = '/health'
+const healthPath = "/health";
 // All items path
-const itemsPath = '/items'
+const itemsPath = "/items";
 // Single item path
-const itemPath = '/item'
+const itemPath = "/item";
 
 // Lambda will start executing code because of exports.handler
 // Event param contains info about upcoming event (f.e API request data) depending on a trigger
 exports.handler = async function (event) {
-  console.log('Request event:', event)
+  console.log("Request event:", event);
 
-  let response
+  let response;
 
   // switch(true) will work as if/else statements.
   switch (true) {
     // If requested method is GET, and requested path was the health one
-    case event.httpMethod === 'GET' && event.path === healthPath:
+    case event.httpMethod === "GET" && event.path === healthPath:
       // save response variable as buildResponse function with status code 200
       // 200 means successfull HTTP status code (f.e 201 is for created, 400 for bad request, 404 not found or 500 for internal server error)
-      response = buildResponse(200)
-      break
+      response = buildResponse(200);
+      break;
     // save response variable as getItem function that uses a ID argument which is 'name'
-    case event.httpMethod === 'GET' && event.path === itemPath:
-      response = await getItem(event.queryStringParameters.name)
-      break
+    case event.httpMethod === "GET" && event.path === itemPath:
+      response = await getItem(event.queryStringParameters.name);
+      break;
     // Same as above, but this time function that has all items will be saved in a response variable
-    case event.httpMethod === 'GET' && event.path === itemsPath:
-      response = await getItems()
-      break
+    case event.httpMethod === "GET" && event.path === itemsPath:
+      response = await getItems();
+      break;
   }
-  return response
-}
+  return response;
+};
 
 // Function that takes certain item from my table ('tickets')
 const getItem = async function (name) {
@@ -49,7 +51,7 @@ const getItem = async function (name) {
     Key: {
       name: name,
     },
-  }
+  };
 
   // Using the dynamoDB get method to read data from the table, based on parameters
   // Then returning a Promise that resolves to the item (item is read from the table) or rejects with an error
@@ -57,15 +59,58 @@ const getItem = async function (name) {
     (response) => {
       // If the Promise resolves successfully (i.e., the item is read without errors)
       // build and return an HTTP response with a 200 status code and the retrieved item
-      return buildResponse(200, response.Item)
+      return buildResponse(200, response.Item);
     },
     (error) => {
       // If the Promise is rejected (i.e., there's an error while reading the item),
       // log the error to the console and return an undefined value
-      console.error('ERROR WITH GETTING THE ITEM:', error)
-    },
-  )
-}
+      console.error("ERROR WITH GETTING THE ITEM:", error);
+    }
+  );
+};
+
+// Function that retrieves all items from my dynamoDB table
+const getItems = async function () {
+  // Parameters needed for scan operation (name of the table)
+  const params = {
+    TableName: dynamoDBTableName,
+  };
+
+  // Calling the 'scanDynamoRecords' function to fetch all the items in the table
+  // The getItems function asks the scanDynamoRecords function for help to get all the items because the DynamoDB scan operation might not return all the items in one go. Sometimes, when there are many items in the table, the scan operation returns only a part of the items and tells you where it stopped. This is called pagination.
+  // The scanDynamoRecords function is designed to handle this pagination. It keeps looking at the items in the table, and if there are more items left to look at, it continues from where it stopped last time. This makes sure that all the items in the table are collected, even if the scan operation returns them in parts.
+  // So, by asking the scanDynamoRecords function for help, the getItems function can focus on its main job, which is to get all the items from the table and send them back as a response. The scanDynamoRecords function takes care of the details related to scanning the table and handling pagination.
+  const allItems = await scanDynamoRecords(params, []);
+
+  // Creating a response body for containing the items
+  const body = {
+    items: allItems,
+  };
+  // Return the response object with a 200 status code and the items in the body
+  return buildResponse(200, body);
+};
+
+// Helper function to scan DynamoDB records recursively (fetches all items in a table)
+const scanDynamoRecords = async function (scanParams, itemArray) {
+  try {
+    // Call the scan operation in Dynamo DB table
+    const dynamoData = await dynamodb.scan(scanParams).promise();
+
+    // Concatenate the items from the current scan operation to the itemArray
+    itemArray = itemArray.concat(dynamoData.Items);
+
+    // If there's a LastEvaluatedKey, it means there are more items to fetch
+    if (dynamoData.LastEvaluateKey) {
+      // Update the ExclusiveStartKey to the LastEvaluateKey to continue scanning
+      // (If there are more items left to look at, it remembers where it stopped and continues looking from there.)
+      scanParams.ExclusiveStartKey = dynamoData.LastEvaluateKey;
+      return await scanDynamoRecords(scanParams, itemArray);
+    }
+    return itemArray;
+  } catch (error) {
+    console.error("ERROR WITH SCANING DYNAMO DB RECORDS", error);
+  }
+};
 
 // This funciton has a purpose to create a response object that conforms to the format expected by API Gateway
 // It takes 2 arguments - statusCode and body
@@ -77,10 +122,10 @@ const buildResponse = function (statusCode, body) {
     // Headers are key-walue pairs that help both the server and the client understand how to handle a response
     headers: {
       // This indicates that the response body will be in JSON format
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     // setting 'body' property of the returned object as a JSON string
     // JSON.stringify converts a JavaScript object or value to a JSON string
     body: JSON.stringify(body),
-  }
-}
+  };
+};
