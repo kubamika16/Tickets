fc864de069a4db51eb16d161d4d4e37f9db7831a
kubamika16 <kuba.mika16@gmail.com>
2023-05-04 14:43:46 +0100

    commit

--- objectChrome.js
+++ objectChrome.js
@@ -62,20 +62,31 @@ console.log(`Event details saved.`);
 // FUNCTION THAT OPENS LINKS
 
 const links = [
-  'https://concerts.livenation.com/idk-santa-ana-california-10-10-2023/event/09005E9303AB49CD',
-  'https://www.ticketmaster.com/panda-bear-sonic-boom-seattle-washington-07-16-2023/event/0F005E949A101BAC',
-  'https://www.ticketmaster.com/rema-rave-roses-north-american-tour-seattle-washington-08-12-2023/event/0F005E956F8911A3',
-  'https://concerts.livenation.com/roar-los-angeles-california-09-13-2023/event/09005E98F680337F',
-  'https://www.ticketmaster.com/morgan-wade-asbury-park-new-jersey-08-16-2023/event/00005E92BB50E616',
-  'https://www.ticketmaster.com/generationals-washington-district-of-columbia-10-14-2023/event/15005E939CAF2BE3',
-  'https://concerts.livenation.com/belly-18-boston-massachusetts-10-05-2023/event/01005E95E0BA6C8D',
-  'https://www.ticketmaster.com/the-gangs-all-here-tour-skid-suquamish-washington-10-06-2023/event/0F005E91A2BA884C',
-  'https://concerts.livenation.com/chris-renzema-with-special-guest-jess-boston-massachusetts-09-30-2023/event/01005E92A23921B2',
-  'https://concerts.livenation.com/event/04005E94BCCC2670',
-  'https://concerts.livenation.com/jidenna-santa-ana-california-07-21-2023/event/09005E7EA4D624C0',
-  'https://concerts.livenation.com/boz-scaggs-summer-23-tour-san-francisco-california-08-26-2023/event/1C005E8DD13055D8',
-  'https://concerts.livenation.com/boz-scaggs-summer-23-tour-san-francisco-california-08-25-2023/event/1C005E8DD12E55D0'
-]
+  "https://concerts.livenation.com/idk-santa-ana-california-10-10-2023/event/09005E9303AB49CD",
+  "https://www.ticketmaster.com/panda-bear-sonic-boom-seattle-washington-07-16-2023/event/0F005E949A101BAC",
+  "https://www.ticketmaster.com/rema-rave-roses-north-american-tour-seattle-washington-08-12-2023/event/0F005E956F8911A3",
+  "https://concerts.livenation.com/roar-los-angeles-california-09-13-2023/event/09005E98F680337F",
+  "https://www.ticketmaster.com/morgan-wade-asbury-park-new-jersey-08-16-2023/event/00005E92BB50E616",
+  "https://www.ticketmaster.com/generationals-washington-district-of-columbia-10-14-2023/event/15005E939CAF2BE3",
+  "https://concerts.livenation.com/belly-18-boston-massachusetts-10-05-2023/event/01005E95E0BA6C8D",
+  "https://www.ticketmaster.com/the-gangs-all-here-tour-skid-suquamish-washington-10-06-2023/event/0F005E91A2BA884C",
+  "https://concerts.livenation.com/chris-renzema-with-special-guest-jess-boston-massachusetts-09-30-2023/event/01005E92A23921B2",
+  "https://concerts.livenation.com/event/04005E94BCCC2670",
+  "https://concerts.livenation.com/jidenna-santa-ana-california-07-21-2023/event/09005E7EA4D624C0",
+  "https://concerts.livenation.com/boz-scaggs-summer-23-tour-san-francisco-california-08-26-2023/event/1C005E8DD13055D8",
+  "https://concerts.livenation.com/boz-scaggs-summer-23-tour-san-francisco-california-08-25-2023/event/1C005E8DD12E55D0",
+  "https://www.ticketmaster.com/ava-max-on-tour-finally-nashville-tennessee-06-12-2023/event/1B005E8E813B2068",
+  "https://www.ticketmaster.com/waka-flocka-flame-nashville-tennessee-05-18-2023/event/1B005E8DF3C7A42F",
+  "https://www.ticketmaster.com/owl-city-nashville-tennessee-10-15-2023/event/1B005E8DD4608F6C",
+  "https://www.ticketmaster.com/crankdat-harrisburg-pennsylvania-11-03-2023/event/02005E9E294CB9EB",
+  "https://www.ticketmaster.com/diaz-music-group-presents-caifanes-nashville-tennessee-06-08-2023/event/1B005E8EDEC99DAB",
+  "https://www.ticketmaster.com/crankdat-albany-new-york-10-28-2023/event/30005E9BDF552DC7",
+  "https://www.ticketmaster.com/future-islands-nashville-tennessee-05-05-2023/event/1B005E8DCEB48D0D",
+  "https://www.ticketmaster.com/declan-mckenna-the-big-return-nashville-tennessee-07-30-2023/event/1B005E8DDC6996B7",
+  "https://www.ticketmaster.com/pouya-nashville-tennessee-05-22-2023/event/1B005E8E76D01610",
+  "https://www.ticketmaster.com/pouya-nashville-tennessee-05-22-2023/event/1B005E8E76D01610",
+  "https://www.ticketmaster.com/less-than-jake-nashville-tennessee-07-06-2023/event/1B005E8E88F32614",
+];
 
 const links2 = [
   "https://www.ticketmaster.com/joy-oladokun-living-proof-tour-charlottesville-virginia-09-23-2023/event/01005E93A29931A2",
