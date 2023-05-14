75b65de3665bfd70f3940464c224638642934da4
kubamika16 <kuba.mika16@gmail.com>
2023-05-14 10:26:56 +0100

    commit

--- objectChrome.js
+++ objectChrome.js
@@ -80,6 +80,8 @@ const links = [
   "https://www.ticketmaster.com/kany-garcia-usa-tour-2023-boston-massachusetts-09-16-2023/event/01005EA58B323D51",
   "https://www.ticketmaster.com/high-tolerance-ent-presents-icewear-vezzo-atlanta-georgia-06-17-2023/event/0E005EA4DD2C6CB2",
   "https://concerts.livenation.com/ashley-metha-san-diego-california-07-21-2023/event/0A005EA5CF5C519D",
+];
+const links2 = [
   "https://www.ticketmaster.com/bassrush-presents-dirt-monkey-washington-district-of-columbia-05-31-2023/event/15005EAA8FF91FB6",
   "https://www.ticketmaster.com/black-country-new-road-los-angeles-california-09-01-2023/event/09005EA9980E2F79",
   "https://www.ticketmaster.com/drumming-bird-andrew-montana-houston-texas-08-09-2023/event/3A005EA80C8E7A94",
@@ -87,8 +89,21 @@ const links = [
   "https://www.ticketmaster.com/the-chainsmokers-everett-massachusetts-06-06-2023/event/01005EA414A7A0F1",
   "https://www.ticketmaster.com/ludacris-music-by-dj-infamous-boston-massachusetts-05-19-2023/event/01005EA978D1125F",
   "https://www.ticketmaster.com/musiq-soulchild-new-york-new-york-05-31-2023/event/00005EA9A90C3C0D",
+  "https://concerts.livenation.com/bowling-for-soup-boston-massachusetts-09-11-2023/event/01005EA3C7765747",
+  "https://www.ticketmaster.com/underoath-official-platinum-detroit-michigan-07-14-2023/event/08005EAC65FA0FDD",
+  "https://concerts.livenation.com/charlotte-cardin-boston-massachusetts-10-25-2023/event/01005E95AE53445A",
+  "https://www.ticketmaster.com/dvbbs-washington-district-of-columbia-07-20-2023/event/15005EAA1D4F7E4B",
+  "https://www.ticketmaster.com/the-outlaws-ponte-vedra-beach-florida-12-10-2023/event/22005EA1A6AFBC35",
+  "https://concerts.livenation.com/dizzy-los-angeles-california-11-15-2023/event/09005EA5ACDA6639",
+  "https://www.ticketmaster.com/above-beyond-boston-massachusetts-06-23-2023/event/01005EA97E2F131F",
+  "https://www.ticketmaster.com/sam-feldt-boston-massachusetts-08-11-2023/event/01005EA41828A220",
+  "https://www.ticketmaster.com/bowling-for-soup-atlanta-georgia-09-05-2023/event/0E005EA19D022591",
+  "https://www.ticketmaster.com/kflay-albany-new-york-08-11-2023/event/30005EA5EA6932DE",
+  "https://www.ticketmaster.com/wolfmother-seattle-washington-09-22-2023/event/0F005EA896A81747",
+  "https://concerts.livenation.com/tim-atlas-philadelphia-pennsylvania-09-19-2023/event/02005EAAABB1660D",
+  "https://concerts.livenation.com/charlotte-cardin-philadelphia-pennsylvania-10-28-2023/event/02005EA1D9586118",
+  "https://concerts.livenation.com/attila-about-that-life-10-year-new-york-new-york-09-12-2023/event/00005E9AEC2575CF",
 ];
-const links2 = [];
 
 async function openLinks() {
   for (let i = 0; i < links.length; i++) {
