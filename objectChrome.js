3051a69b40e3197943457092c178f7ef05b33474
kubamika16 <kuba.mika16@gmail.com>
2023-05-20 08:57:39 +0100

    commit

--- objectChrome.js
+++ objectChrome.js
@@ -62,27 +62,13 @@ console.log(`Event details saved.`);
 // FUNCTION THAT OPENS LINKS
 
 const links = [
-  "https://www.ticketmaster.com/owl-city-nashville-tennessee-10-15-2023/event/1B005E8DD4608F6C",
-  "https://www.ticketmaster.com/dramarama-asbury-park-new-jersey-08-12-2023/event/00005EA4A463C6CD",
   "https://concerts.livenation.com/have-a-nice-life-chicago-illinois-08-24-2023/event/04005EA8B765183C",
-  "https://concerts.livenation.com/insomniac-presents-william-black-18-santa-ana-california-08-18-2023/event/09005EA8DC9E4653",
-  "https://concerts.livenation.com/yngwie-malmsteen-sacramento-california-09-07-2023/event/1C005EAA8F2028DD",
   "https://www.ticketmaster.com/black-country-new-road-los-angeles-california-09-01-2023/event/09005EA9980E2F79",
-  "https://www.ticketmaster.com/the-chainsmokers-everett-massachusetts-06-06-2023/event/01005EA414A7A0F1",
-  "https://www.ticketmaster.com/ludacris-music-by-dj-infamous-boston-massachusetts-05-19-2023/event/01005EA978D1125F",
-  "https://concerts.livenation.com/bowling-for-soup-boston-massachusetts-09-11-2023/event/01005EA3C7765747",
-  "https://concerts.livenation.com/charlotte-cardin-boston-massachusetts-10-25-2023/event/01005E95AE53445A",
-  "https://www.ticketmaster.com/bowling-for-soup-atlanta-georgia-09-05-2023/event/0E005EA19D022591",
   "https://www.ticketmaster.com/wolfmother-seattle-washington-09-22-2023/event/0F005EA896A81747",
-  "https://concerts.livenation.com/charlotte-cardin-philadelphia-pennsylvania-10-28-2023/event/02005EA1D9586118",
   "https://concerts.livenation.com/toosii-naujour-tour-chicago-illinois-08-30-2023/event/04005EAC18654F8A",
   "https://www.ticketmaster.com/concrete-jungle-ent-presents-red-store-los-angeles-california-07-01-2023/event/09005EACDDB05FB3",
-  "https://www.ticketmaster.com/bar-italia-madison-wisconsin-12-13-2023/event/07005EAA83860E37",
   "https://concerts.livenation.com/bar-italia-boston-massachusetts-12-05-2023/event/01005EA9F2519CCB",
-  "https://www.ticketmaster.com/bar-italia-new-york-new-york-12-04-2023/event/00005EA9FB2D9088",
-  "https://www.ticketmaster.com/dogwood-tales-with-deau-eyes-and-charlottesville-virginia-06-09-2023/event/01005EAE8E9B3288",
   "https://www.ticketmaster.com/tom-sandoval-the-most-extras-cincinnati-ohio-05-31-2023/event/16005EACA9B54126",
-  "https://www.ticketmaster.com/death-warmed-over-covington-kentucky-07-29-2023/event/16005EACC49B56B8",
   "https://www.ticketmaster.com/black-sherif-atlanta-georgia-05-24-2023/event/0E005EAFE63548C2",
   "https://www.ticketmaster.com/los-cafres-atlanta-georgia-08-19-2023/event/0E005EACEAE25875",
 ];
@@ -94,7 +80,7 @@ async function openLinks() {
       setTimeout(() => {
         window.open(links[i], "_blank");
         resolve();
-      }, 12000);
+      }, 10000);
     });
   }
 }
