4bb88409eeb4f72eff6d18d71f3fa1506d452827
kubamika16 <kuba.mika16@gmail.com>
2023-06-01 15:52:08 +0100

    commit

--- objectChrome.js
+++ objectChrome.js
@@ -68,8 +68,6 @@ const links = [
   "https://concerts.livenation.com/koe-wetzel-vip-mg-experience-woodlands-texas-06-10-2023/event/3A005EB799373350",
   "https://concerts.livenation.com/vision-video-santa-ana-california-08-12-2023/event/09005EB13F2389A2",
   "https://concerts.livenation.com/klub-nocturno-rockero-night-80s-goth-los-angeles-california-06-16-2023/event/09005EB436DB0B40",
-  "https://concerts.livenation.com/yella-beezy-santa-ana-california-06-28-2023/event/09005EB7D21241E9",
-  "https://www.ticketmaster.com/k-camp-tempe-arizona-06-30-2023/event/19005EB5AC1A1D99",
   "https://www.ticketmaster.com/saint-motel-seattle-washington-09-26-2023/event/0F005EABF03A9299",
   "https://www.ticketmaster.ca/blackberry-smoke-live-in-concert-ottawa-ontario-07-13-2023/event/31005EB7BBFE19FE",
   "https://concerts.livenation.com/saint-motel-the-awards-show-san-francisco-california-09-23-2023/event/1C005EA9AF864FF4",
@@ -77,27 +75,19 @@ const links = [
   "https://concerts.livenation.com/saint-motel-the-awards-show-san-diego-california-09-19-2023/event/0B005EB1C1672951",
   "https://concerts.livenation.com/saint-motel-the-awards-show-santa-ana-california-09-20-2023/event/09005EB0E508A93A",
   "https://concerts.livenation.com/blessthefall-hollow-bodies-10-year-anniversary-chicago-illinois-08-12-2023/event/04005EB711EC502D",
-];
-const links2 = [
   "https://www.ticketmaster.ca/blackberry-smoke-london-ontario-07-11-2023/event/10005EB7C4EC231F",
   "https://www.ticketmaster.ca/blackberry-smoke-live-in-concert-toronto-ontario-07-12-2023/event/10005EB7CCDF2656",
-  "https://www.ticketmaster.com/buckcherry-jim-thorpe-pennsylvania-09-17-2023/event/02005EB396A444D0",
   "https://www.ticketmaster.com/saint-motel-the-awards-show-austin-texas-09-14-2023/event/3A005EA8BA9C32A5",
   "https://concerts.livenation.com/saint-motel-the-awards-show-sacramento-california-09-24-2023/event/1C005EB127AD9694",
   "https://concerts.livenation.com/blessthefall-hollow-bodies-10-year-anniversary-santa-ana-california-08-03-2023/event/09005EB6F4473EFF",
-  "https://concerts.livenation.com/josh-abbott-band-salt-lake-city-utah-09-13-2023/event/1E005EB2D9804534",
-  "https://www.ticketmaster.com/the-airborne-toxic-event-new-york-new-york-08-05-2023/event/00005EB6AA5738C5",
   "https://www.ticketmaster.ca/rafsaperra-toronto-ontario-07-15-2023/event/10005EB3CB11366D",
-  "https://concerts.livenation.com/event/09005EA247719EC1",
-  "https://www.ticketmaster.com/starbenders-the-haunt-albany-new-york-06-30-2023/event/30005EB6F764253B",
   "https://concerts.livenation.com/blessthefall-hollow-bodies-10-year-anniversary-new-york-new-york-08-18-2023/event/00005EB7B9434821",
-  "https://www.ticketmaster.com/event/15005EB2A57C3843",
-  "https://www.ticketmaster.com/ill-peach-los-angeles-california-07-13-2023/event/09005EB9E02748D3",
-  "https://www.ticketmaster.com/chris-cagle-hampton-new-hampshire-08-24-2023/event/01005EB986BF1706",
-  "https://concerts.livenation.com/k-camp-raleigh-north-carolina-07-26-2023/event/2D005EB70E5E9A15",
-  "https://concerts.livenation.com/k-camp-silver-spring-maryland-07-23-2023/event/15005EB623B467E8",
-  "https://www.ticketmaster.com/motionless-in-white-orlando-florida-10-15-2023/event/22005EB0D846CE54",
+  "https://www.ticketmaster.com/ridgeway-los-angeles-california-06-22-2023/event/09005EB3C7B96CFE",
+  "https://www.ticketmaster.com/mass-destruction-metal-fest-sunday-only-atlanta-georgia-11-05-2023/event/0E005EB6C7043636",
+  "https://www.ticketmaster.com/mass-destruction-metal-fest-friday-only-atlanta-georgia-11-03-2023/event/0E005EB6C5AE35DC",
+  "https://www.ticketmaster.com/mass-destruction-metal-fest-saturday-only-atlanta-georgia-11-04-2023/event/0E005EB6C5E735EA",
 ];
+const links2 = [];
 
 async function openLinks() {
   for (let i = 0; i < links.length; i++) {
