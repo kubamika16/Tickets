2ee1486544223cfebb9e0ebe99d72ffc39187ee1
kubamika16 <kuba.mika16@gmail.com>
2023-06-04 11:09:02 +0100

    commit

--- objectChrome.js
+++ objectChrome.js
@@ -80,19 +80,31 @@ const links = [
   "https://concerts.livenation.com/freak-show-jordana-with-dev-lemons-santa-ana-california-10-06-2023/event/09005EB3F77C8B89",
   "https://concerts.livenation.com/pluko-los-angeles-california-09-08-2023/event/09005EBA4D8785A4",
   "https://concerts.livenation.com/akthesavior-los-angeles-california-07-19-2023/event/09005EBF9E672B2B",
+];
+const links2 = [
   "https://concerts.livenation.com/freak-show-jordana-with-dev-lemons-boston-massachusetts-10-20-2023/event/01005EB2E98982E0",
   "https://concerts.livenation.com/miya-folick-the-roach-tour-san-diego-california-09-22-2023/event/0A005EB2B791314A",
   "https://www.ticketmaster.com/pluko-new-york-new-york-09-23-2023/event/00005EBACA93983A",
   "https://www.ticketmaster.com/chris-webby-hampton-new-hampshire-07-27-2023/event/01005EBA8AF81CAC",
   "https://www.ticketmaster.com/the-happy-fits-under-the-shade-tucson-arizona-10-06-2023/event/19005EAFAA321D70",
   "https://www.ticketmaster.com/chris-webby-portland-maine-07-28-2023/event/01005EBA8C1F1CDF",
-  "https://www.ticketmaster.com/chris-webby-portland-maine-07-28-2023/event/01005EBA8C1F1CDF",
   "https://www.ticketmaster.com/the-happy-fits-albany-new-york-09-20-2023/event/30005EB2BF541D2D",
   "https://www.ticketmaster.com/the-happy-fits-albany-new-york-09-20-2023/event/30005EB2BF541D2D",
   "https://www.ticketmaster.com/the-happy-fits-under-the-shade-charlottesville-virginia-10-24-2023/event/01005EB89EA54294",
   "https://www.ticketmaster.com/the-happy-fits-under-the-shade-charlottesville-virginia-10-24-2023/event/01005EB89EA54294",
+  "https://www.ticketmaster.com/foreigner-farewell-tour-bethlehem-pennsylvania-11-18-2023/event/02005EB9DF985245",
+  "https://www.ticketmaster.com/foreigner-farewell-tour-bethlehem-pennsylvania-11-17-2023/event/02005EB9DF8F5242",
+  "https://www.ticketmaster.com/puddle-of-mudd-rialto-theatre-tucson-arizona-09-03-2023/event/19005EBABACF2535",
+  "https://www.ticketmaster.com/kesha-new-orleans-louisiana-10-18-2023/event/1B005EB7AE462799",
+  "https://concerts.livenation.com/boys-like-girls-the-speaking-our-salt-lake-city-utah-10-13-2023/event/1E005EB886F128E2",
+  "https://www.ticketmaster.com/black-veil-brides-vv-tour-2023-huntington-new-york-09-25-2023/event/00005EB7A98D2DB8",
+  "https://concerts.livenation.com/boys-like-girls-the-speaking-our-las-vegas-nevada-10-02-2023/event/17005EB3FA5F9458",
+  "https://concerts.livenation.com/the-japanese-house-san-diego-california-11-17-2023/event/0B005EB8E43C3699",
+  "https://www.ticketmaster.com/the-guess-who-atlantic-city-new-jersey-09-23-2023/event/02005EB2AC7B33F3",
+  "https://www.ticketmaster.com/the-japanese-house-washington-district-of-columbia-11-04-2023/event/15005EB6C1F62DF6",
+  "https://www.ticketmaster.com/boys-like-girls-the-speaking-our-nashville-tennessee-09-18-2023/event/1B005EB3D8297258",
+  "https://concerts.livenation.com/pop-evil-skeletons-tour-new-orleans-louisiana-07-26-2023/event/1B005EB9DB7138D2",
 ];
-const links2 = [];
 
 async function openLinks() {
   for (let i = 0; i < links.length; i++) {
